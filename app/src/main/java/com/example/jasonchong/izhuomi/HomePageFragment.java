adc5623d5174f2dd2b84d29276afa320716e8659
jasonchong0401 <jason0401chong@gmail.com>
2015-04-04 21:55:11 +0800

    add ReadingFragment & its layout

--- app/src/main/java/com/example/jasonchong/izhuomi/HomePageFragment.java
+++ app/src/main/java/com/example/jasonchong/izhuomi/HomePageFragment.java
@@ -1,6 +1,5 @@
 package com.example.jasonchong.izhuomi;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,7 +12,7 @@ import android.view.ViewGroup;
 public class HomePageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.layout3, container, false);
+        return inflater.inflate(R.layout.fragment_homepage, container, false);
     }
 
     @Override
--- app/src/main/java/com/example/jasonchong/izhuomi/NoteFragment.java
+++ app/src/main/java/com/example/jasonchong/izhuomi/NoteFragment.java
@@ -1,6 +1,5 @@
 package com.example.jasonchong.izhuomi;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -13,7 +12,7 @@ import android.view.ViewGroup;
 public class NoteFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.layout2, container, false);
+        return inflater.inflate(R.layout.fragment_note, container, false);
     }
 
     @Override
